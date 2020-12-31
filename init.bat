d2aca67f95e4e29b9db820cde9ac439418960550
inugami-dev64 <jxwest@protonmail.com>
2020-12-31 13:46:05 +0200

    Add freetype2 source repository as submodule

---
+++ init.bat
@@ -0,0 +1,11 @@
+@echo off
+
+if not exist %1 (mkdir %1)
+if not exist %1\obj (mkdir %1\obj)
+if not exist %1\deng (mkdir %1\deng)
+if not exist %1\deng\lib (mkdir %1\deng\lib)
+if not exist %1\obj\libsurface (mkdir %1\obj\libsurface)
+if not exist %1\obj\libdas (mkdir %1\obj\libdas)
+if not exist %1\obj\libcommon (mkdir %1\obj\libcommon)
+if not exist %1\obj\dam (mkdir %1\obj\dam)
+if not exist %1\obj\deng (mkdir %1\obj\deng)
